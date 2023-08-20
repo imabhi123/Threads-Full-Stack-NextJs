cfd9f02ddb7226f9254f9fed5660b984b8927d7d
Harry <kumarabhishek13909@gmail.com>
2023-08-20 05:37:36 +0530

    initial commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,35 @@
-//app/page.tsx
-import { UserButton } from "@clerk/nextjs/app-beta"
+import ThreadCard from "@/components/cards/ThreadCard";
+import { fetchPosts } from "@/lib/actions/thread.actions";
+import { currentUser } from "@clerk/nextjs";
 
-export default function Home() {
+export default async function Home() {
+  const result = await fetchPosts(1, 30);
+  const user = await currentUser();
+  // console.log(user)
   return (
     <div>
       <h1 className="text-white">Home</h1>
+      <section className="mt-9 flex flex-col gap-10">
+        {result.posts.length === 0 ? (
+          <p className="no-result">No Threads found</p>
+        ) : (
+          <>
+            {result.posts.map((post) => {
+              return(<ThreadCard
+                key={post._id}
+                id={post._id}
+                currentUserId={user?.id||''}
+                parentId={post.parentId}
+                author={post.author}
+                content={post.text}
+                community={post.community}
+                createdAt={post.createdAt}
+                comments={post.children}
+              />);
+            })}
+          </>
+        )}
+      </section>
     </div>
-  )
+  );
 }
---
+++ app/(root)/thread/[id]/page.tsx
@@ -0,0 +1,42 @@
+import ThreadCard from "@/components/cards/ThreadCard";
+import { fetchThreadById } from "@/lib/actions/thread.actions";
+import { fetchUser } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import Comment from "@/components/forms/Comment";
+
+const Page = async ({ params }: { params: { id: string } }) => {
+  if (!params.id) return null;
+  const user = await currentUser();
+  if (!user) return null;
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+  const thread=await fetchThreadById(params.id);
+
+  return (
+    <section className="relative">
+      <div>
+        <ThreadCard
+          key={thread._id}
+          id={thread._id}
+          currentUserId={user?.id || ""}
+          parentId={thread.parentId}
+          author={thread.author}
+          content={thread.text}
+          community={thread.community}
+          createdAt={thread.createdAt}
+          comments={thread.children}
+        />
+      </div>
+      <div className="mt-7">
+        <Comment
+        threadId={thread.id}
+        currentUserImg={user.imageUrl}
+        currentUserId={JSON.stringify(userInfo._id)}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Page;
