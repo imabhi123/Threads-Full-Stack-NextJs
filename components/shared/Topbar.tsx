1000bcf2cf2d1f613c0a5f72b4f5cdc768851c9d
Harry <kumarabhishek13909@gmail.com>
2023-08-22 05:59:53 +0530

    project completed

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -2,12 +2,13 @@ import { OrganizationSwitcher, SignedIn, SignOutButton } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import Image from "next/image";
 import Link from "next/link";
-
+import imgg from '../../public/assets/logo.svg'
+import { Limelight } from "next/font/google";
 function Topbar() {
   return (
     <nav className="topbar">
       <Link href="/" className="flex items-center gap-4">
-        <Image src="/assets/logo.svg" alt="logo" width={28} height={28} />
+        <Image src={imgg} alt="logo" width={28} height={28} />
         <p className="text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
       </Link>
 
